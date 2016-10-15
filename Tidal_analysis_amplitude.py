64bda54887f2c6ebf1fa2e639851c5f51a0e077d
Karen Guihou <karen.guihou@gmail.com>
2016-10-15 18:27:27 +0200

    working version

--- Tidal_analysis_amplitude.py
+++ Tidal_analysis_amplitude.py
@@ -41,31 +41,26 @@ use_bathy = config.use_bathy
 bathyvar = config.bathyvar
 min_depth = config.min_depth
 constituents = config.constituents
-
+filetype = config.filetype
 
 ## Functions
 
-def readMODELnc(filename, var):
+def readMODEL(filename, var):
     """
     Read a variable from a NEMO output (netcdf 3 or 4)
     """
-
-    f = netcdf.netcdf_file(filename, 'r')
-    data = f.variables[var].data
-    f.close()
-
-    return data
-
-
-def readMODELhdf5(filepath, var):
-    """
-    Read a variable from a NEMO output (hdf5)
-    """
-
-    f = h5py.File(filepath, 'r')
-    variable = (f[var])[:]
-    f.close()
-    return variable
+    if filetype == 'nc':
+        f = netcdf.netcdf_file(filename, 'r')
+        data = f.variables[var].data
+        f.close()
+        return data
+    elif filetype == 'hdf5':
+        f = h5py.File(filename, 'r')
+        variable = (f[var])[:]
+        f.close()
+        return variable
+    else:
+        print('unknown filetype. Is it something else that hdf5 or nc?')
 
 
 def do_kdtree(combined_x_y_arrays, points):
@@ -73,9 +68,10 @@ def do_kdtree(combined_x_y_arrays, points):
     (dist, indexes) = mytree.query(points)
     return indexes
 
-def filter_invalid_cols(d, idx):
+def filter_invalid_cols(d, idx, ntype=float):
     data = np.zeros(len(idx))
-    data = [ d[x] for x in idx ]
+    data = np.array([ d[x] for x in idx ],dtype=ntype)
+
     return data
 
 ## Read the coordinates and masks
@@ -90,13 +86,13 @@ coordobs = np.transpose(np.concatenate((lonobs, latobs)).reshape(2,
 
 # Model
 
-lonmod = readMODELhdf5(paths['msk'], 'nav_lon').flatten()
-latmod = readMODELhdf5(paths['msk'], 'nav_lat').flatten()
+lonmod = readMODEL(paths['msk'], 'nav_lon').flatten()
+latmod = readMODEL(paths['msk'], 'nav_lat').flatten()
 coordmod = np.transpose(np.concatenate((lonmod, latmod)).reshape(2,
                         len(lonmod)))
-mask = readMODELhdf5(paths['msk'], mskvar)[0, 0, :, :].flatten()
+mask = readMODEL(paths['msk'], mskvar)[0, 0, :, :].flatten()
 if use_bathy == 1:
-    bathy = readMODELhdf5(paths['bathy'], bathyvar).flatten()
+    bathy = readMODEL(paths['bathy'], bathyvar).flatten()
 
 ## Get indexes of model grid points where there are observations. Remove data located inland.
 
@@ -104,12 +100,9 @@ ind = do_kdtree(coordmod, coordobs)
 counter = 0
 indmod = np.zeros(len(ind), dtype=int)
 indobs = np.zeros(len(ind), dtype=int)
-for idx, val in enumerate(inc):
+for idx, val in enumerate(ind):
     if use_bathy == 0:
         if mask[val] == 1:
-
-        # if abs((indmod-val)).min() != 0 : # remove double points? With that method, keeps the first tide gauge, not a mean
-
             indmod[counter] = val
             indobs[counter] = idx
             counter += 1
@@ -127,10 +120,10 @@ if verbose:
 
 ## Save the filtrered coordinates.
 
-latobs_filt = latobs[indobs]
-lonobs_filt = lonobs[indobs]
-latmod_filt = latmod[indmod]
-lonmod_filt = lonmod[indmod]
+latobs = latobs[indobs]
+lonobs = lonobs[indobs]
+latmod = latmod[indmod]
+lonmod = lonmod[indmod]
 
 ## Loop over constituents, extract amplitude and phase for each
 
@@ -149,51 +142,49 @@ for const in constituents:
     amplobs_filt = amplobs[indobs]
     phaobs_filt = phaobs[indobs]
 
-    # Testing data, just in case...
-    if not len(amplobs_filt) == len(phaobs_filt) == len(lonobs_filt) == len(latobs_filt) == len(lonmod_filt) == len(latmod_filt) : #== len(indmod) ?
-        sys.exit("There is something wrong with data for const %s" % const)
-        continue
-
-    data_length = len(amplobs_filt) # become useless unless you reuse it
 
     # second filtering, for amplobs = 9999
 
     valid_idx = []
-    for idx, lat_obs_val in enumerate(latobs_filt):
-        if lat_obs_val != 9999:
+    for idx, val_obs_val in enumerate(amplobs_filt):
+        if int(val_obs_val) != 9999 :
             valid_idx.append(idx)
     amplobs_filt = filter_invalid_cols( amplobs_filt, valid_idx )
-    phaobs_filt = filter_invalid_cols( amplobs_filt, valid_idx )
-    lonobs_filt = filter_invalid_cols( amplobs_filt, valid_idx )
-    latobs_filt = filter_invalid_cols( amplobs_filt, valid_idx )
-    lonmod_filt = filter_invalid_cols( amplobs_filt, valid_idx )
-    latmod_filt = filter_invalid_cols( amplobs_filt, valid_idx )
-    indmod_filt = filter_invalid_cols( amplobs_filt, valid_idx )
+    phaobs_filt = filter_invalid_cols( phaobs_filt, valid_idx )
+    lonobs_filt = filter_invalid_cols( lonobs, valid_idx )
+    latobs_filt = filter_invalid_cols( latobs, valid_idx )
+    lonmod_filt = filter_invalid_cols( lonmod, valid_idx )
+    latmod_filt = filter_invalid_cols( latmod, valid_idx )
+    indmod_filt = filter_invalid_cols( indmod, valid_idx , int)
+
+    # Testing data, just in case...
+    if not len(amplobs_filt) == len(phaobs_filt) == len(lonobs_filt) == len(latobs_filt) == len(lonmod_filt) == len(latmod_filt) : #== len(indmod) ?
+        sys.exit("There is something wrong with data for const %s" % const)
 
     # Model
 
-    xval = readMODELhdf5(paths['model'], str(const) + 'x'
+    xval = readMODEL(paths['model'], str(const) + 'x'
                          ).flatten()
-    yval = readMODELhdf5(paths['model'], str(const) + 'y'
+    yval = readMODEL(paths['model'], str(const) + 'y'
                          ).flatten()
-    xval_filt = xval[indmod_filt2]
-    yval_filt = yval[indmod_filt2]
-    amplmod_filt2 = np.sqrt(np.square(xval_filt) + np.square(yval_filt))
+    xval_filt = xval[indmod_filt]
+    yval_filt = yval[indmod_filt]
+    amplmod_filt = np.sqrt(np.square(xval_filt) + np.square(yval_filt))
 
     # Stats
 
-    N = counter
-    rmse = np.sqrt(np.sum(np.square(amplmod_filt2 * 100 - amplobs_filt2
-                   * 100)) * 1 / N)
-    mean = np.sum(amplmod_filt2 * 100 - amplobs_filt2 * 100) * 1 / N
+    N = len(valid_idx)
+    rmse = np.sqrt(np.sum(np.square(amplmod_filt * 100 - amplobs_filt
+                   * 100)) /N )
+    mean = np.sum(amplmod_filt * 100 - amplobs_filt * 100) / N
     if verbose == 2:
         print ('N = ', N)
         print ('RMSE = ', rmse)
         print ('MEAN = ', mean)
 
-    np.savetxt(const + '_latobs.txt', latobs_filt2)
-    np.savetxt(const + '_lonobs.txt', lonobs_filt2)
-    np.savetxt(const + '_latmod.txt', latmod_filt2)
-    np.savetxt(const + '_lonmod.txt', lonmod_filt2)
-    np.savetxt(const + '_amplobs.txt', amplobs_filt2)
-    np.savetxt(const + '_amplmod.txt', amplmod_filt2)
+    np.savetxt(const + '_latobs.txt', latobs_filt)
+    np.savetxt(const + '_lonobs.txt', lonobs_filt)
+    np.savetxt(const + '_latmod.txt', latmod_filt)
+    np.savetxt(const + '_lonmod.txt', lonmod_filt)
+    np.savetxt(const + '_amplobs.txt', amplobs_filt)
+    np.savetxt(const + '_amplmod.txt', amplmod_filt)
